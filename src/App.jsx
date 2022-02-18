d261187d7fd0aad3c8d5ad9510ac4676fbcf6bd7
Artem <artemosadach@gmail.com>
2022-02-18 03:40:15 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,16 @@ import Header from './Components/HeaderComponent/Header'
 import Content from './Components/ContentComponent/Content'
 import { useState } from 'react/cjs/react.development';
 import {arrayRus, arrayEng} from './Utils/Words'
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   
   const [language, setLanguage] = useState("Ru")
 
   return (
-    <Routes>
-       <Route path="/" element={<div className={s.page} >
+  <div className={s.page} >
           <Header />
           <Content language={language} setLanguage={setLanguage}  array={language === 'Ru' ? arrayRus : arrayEng } />
-        </div>} /> 
-      
-    </Routes>
+        </div>
   );
 }
 
