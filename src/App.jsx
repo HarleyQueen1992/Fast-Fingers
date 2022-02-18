3ba30a04c409993e2aaaf97bfa9311e2d735706b
Artem <artemosadach@gmail.com>
2022-02-18 03:08:59 +0300

    Route Asia

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ import Header from './Components/HeaderComponent/Header'
 import Content from './Components/ContentComponent/Content'
 import { useState } from 'react/cjs/react.development';
 import {arrayRus, arrayEng} from './Utils/Words'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   
   const [language, setLanguage] = useState("Ru")
 
   return (
-    <div className={s.page} >
-      <Header />
-      <Content language={language} setLanguage={setLanguage}  array={language === 'Ru' ? arrayRus : arrayEng } />
-    </div>
+    <Routes>
+       <Route path="/" element={<div className={s.page} >
+          <Header />
+          <Content language={language} setLanguage={setLanguage}  array={language === 'Ru' ? arrayRus : arrayEng } />
+        </div>} /> 
+      
+    </Routes>
   );
 }
 
