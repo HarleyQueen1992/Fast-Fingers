d1ad8a1495249f2d306153d4a7591b15b2c34a05
Artem <artemosadach@gmail.com>
2022-02-18 04:05:04 +0300

    Fix bug import react

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
+import React, { useState } from 'react'
 import s from  './App.module.css';
 import Header from './Components/HeaderComponent/Header'
 import Content from './Components/ContentComponent/Content'
-import { useState } from 'react/cjs/react.development';
 import {arrayRus, arrayEng} from './Utils/Words'
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   const [language, setLanguage] = useState("Ru")
 
   return (
-  <div className={s.page} >
+        <div className={s.page} >
           <Header />
           <Content language={language} setLanguage={setLanguage}  array={language === 'Ru' ? arrayRus : arrayEng } />
         </div>
